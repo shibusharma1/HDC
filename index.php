8b3f75e3c54b25e3b3c34d70bd2875bb9400da7c
kajol3 <mehtakajol21@gmail.com>
2024-06-12 22:54:59 +0545

    css

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Index</title>
 </head>
 <body>
 <header>
@@ -11,6 +11,7 @@
         <div class="nav-logo border">
             <div class="logo"></div>
         </div> 
+        <a href="login.php">Login</a>
 </div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -4,37 +4,38 @@
   <meta charset="UTF-8">
   <meta http-equiv="x-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login Form in HTML and CSS |Codehal</title>
-  <link rel="stylesheet" href="style.css">
+  <title>Login | Codehal</title>
+  <link rel="stylesheet" href="css/main.css">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
-  <div class"wrapper">
-    <form action=""
+  <div class="wrapper">
+    <form action="" method="POST">
       <h1>Login</h1>
       <div class="input-box">
-        <input type="username"
-        placeholder="username" required>
+        <label for="username">Username</label>
+        <input type="username" placeholder="username" name="username" required>
         <box-icon type='solid' name='user'></box-icon>
 
         </div>
       <div class="input-box">
-        <input type="password"
-        placeholder="password" required>
+        <label for="password">Password</label>
+        <input type="password" placeholder="password" name="password" required>
         <box-icon name='lock'></box-icon>
 </div>
 <div class="remember-forget">
-  <label><input type="checkbox"> Remember me</label>
+  <label><input type="checkbox" required> Remember me</label>
   <a href="#">Forget password?</a>
 </div>
+
 <button type="submit" class="btn">Login</button>
 
   <div class="register-link">
     <p>Don't have an account? <a href="#">Register</a></p>
-  
-    
   </div>
+
 </form>
 </div>
 </body>
 </html>
+
