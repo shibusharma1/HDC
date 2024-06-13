3227ada67880080780824450910e882e382e5e5f
kajol3 <mehtakajol21@gmail.com>
2024-06-13 23:21:54 +0545

    css.register

---
+++ register/student_registration.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/register.css">
+    <title>Form </title>
+</head>
+<body>
+    <div class="container">
+   <div class="wrapper">
+    <form action="" method="POST">
+  
+    <img src="../assets/logo.png" >
+    <h1>Himalaya Darshan Form</h1>
+      <p>please enter your details into the fields below.</p>
+
+      <div class="input-box">
+          <label for="firstname">FIRST NAME</label><br></br>
+          <input type="firstname" placeholder="firstname" name="firstname" required>
+      </div><br></br>
+      <div class="input-box">
+          <label for="middlename">MIDDLE NAME</label><br></br>
+          <input type="middletname" placeholder="middlename" name="middlename" required>
+      </div><br></br>
+      <div class="input-box">
+        <label for="lastname">LAST NAME</label><br></br>
+        <input type="lastname" placeholder="lastname" name="lastname" required>
+      </div><br></br>
+      <div class="input-box">
+        <label for="email">EMAIL</label><br></br>
+        <input type="email" placeholder="email" name="email" required>
+      </div><br></br> 
+      <div class="input-box">
+        <label for="contact number">CONTACT NUMBER</label><br></br>
+        <input type="phone number" placeholder="phone number" name="phone number" required>
+      </div><br></br>
+      <div class="input-box">
+        <label for="programs">PROGRAM</label><br></br>
+        <input type="programs" placeholder="programs" name="programs" required>
+      </div><br></br>
+      <button type="submit" class="btn">Apply</button>
+
+<div class="register-link">
+  <p>Already have an account? <a href="#">Login Here</a></p>
+</div>
+</body>
+</html>
