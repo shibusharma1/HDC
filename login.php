9cce463ec33892310da558c20741df10c7ceec0b
shibusharma1 <shibusharma807@gmail.com>
2024-06-15 22:25:17 +0545

    title ammended

--- login.php
+++ login.php
@@ -63,7 +63,7 @@ include_once 'includes/header.php';
   <meta charset="UTF-8">
   <meta http-equiv="x-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login | Codehal</title>
+  <title>Login | HDC</title>
   <link rel="stylesheet" href="css/login.css">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
