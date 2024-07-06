ae25c2530484c0dd77269a781042fa621f5a8a8d
shibusharma1 <shibusharma807@gmail.com>
2024-07-06 23:31:18 +0545

    landing page done

--- index.php
+++ index.php
@@ -42,25 +42,10 @@ if (isset($_SESSION['uid'])) {
     </div>
     <br>
     <nav class="navbar">
-
-
-        <!-- <input type="checkbox" id="navbar-toggle" class="navbar-checkbox"> -->
-        <!-- <label for="navbar-toggle" class="navbar-toggle">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-        </label> -->
         <ul class="navbar-links">
             <li><a href="index.php">Home</a></li>
             <li><a href="register.php">Register</a></li>
-            <!-- <li class="dropdown">
-                <a href="#" class="dropbtn">Departments</a>
-                <div class="dropdown-content">
-                    <a href="#">Science</a>
-                    <a href="#">Mathematics</a>
-                    <a href="#">Arts</a>
-                </div>
-            </li> -->
+
             <li><a href="contact.php">contact</a></li>
             <li><a href="vote.php">Vote</a></li>
             <li><a href="about.php">About us</a></li>
@@ -80,26 +65,7 @@ if (isset($_SESSION['uid'])) {
         </tr>
         <tr>
             <td class="std-img">
-                <img src="assets/limadidi.png" alt="limadidi" width="50%">
-            </td>
-            <td class=std-description>
-                <p class="std-name"> Lima Sapkota</p>
-                <br>
-                <br>
-                Himalaya Darshan College is the place where student voices are heard and counted. It is difficult to sum
-                up the memories and experience of four years in a few lines. HDC has helped me to develop a positive
-                attitude towards my studies and discover more about myself. Apart from my study, I have been involved
-                with various community groups where I used my academic and professional skills to yield good outcomes. I
-                have grown both personally and professionally.
-
-            </td>
-        </tr>
-        <tr class="table-row">
-            <td colspan="2" class="table-title">Program We Offer:</td>
-        </tr>
-        <tr>
-            <td class="std-img">
-                <img src="assets/limadidi.png" alt="limadidi" width="px">
+                <img src="assets/limadidi.png" alt="limadidi" width="350px">
             </td>
             <td class=std-description>
                 <p class="std-name"> Lima Sapkota</p>
@@ -114,6 +80,75 @@ if (isset($_SESSION['uid'])) {
             </td>
         </tr>
     </table>
+    <div class="program-title">
+        <p> Programs We Offer
+    </div>
+    <div class="program-section">
+        <div class="box1 box">
+            <div class="box-content">
+                <h2>BIM</h2>
+                <div class="box-img" style="background-image:url('assets/bim.png');"></div>
+            </div>
+        </div>
+        <div class="box2 box">
+            <div class="box-content">
+                <h2>BCA</h2>
+                <div class="box-img" style="background-image: url('assets/bca.png');"></div>
+
+            </div>
+        </div>
+        <div class="box3 box">
+            <div class="box-content">
+                <h2>Bsc. CSIT</h2>
+                <div class="box-img" style="background-image: url('assets/bsccsit.png');"></div>
+
+            </div>
+        </div>
+        <div class="box4 box">
+            <div class="box-content">
+                <h2>BHM</h2>
+                <div class="box-img" style="background-image: url('assets/bhm.png');"></div>
+
+            </div>
+        </div>
+        <div class="box5 box">
+            <div class="box-content">
+                <h2>BSS</h2>
+                <div class="box-img" style="background-image: url('assets/bbs.png');"></div>
+
+            </div>
+        </div>
+    </div>
+    <footer>
+
+        <div class="foot-panel1">
+            <ul>
+                <p>Contact us</p>
+                <a href="#">Email</a>
+                <a href="mailto:himalayadarshan5@gmail.com">himalayadarshan5@gmail.com</a>
+                <a href="#">Phone Number:</a>
+                <a href="tel:021-590471">021-590471</a>
+
+                <a href="#">Address</a>
+                <a href="#"> Main Road,Biratnagar-09</a>
+
+            </ul>
+            <ul>
+                <p>Follow us</p>
+                <a href="https://www.facebook.com/215660679312132?ref=embed_page" target="_blank"><img
+                        src="assets/facebook.png" height="50" width="50" alt=""></a>
+
+            </ul>
+        </div>
+
+
+        <div class="foot-panel2">
+            <div class="copyright">
+                Copyright &copy; 2019, All rights reserved. Himalaya Darshan College.
+            </div>
+        </div>
+    </footer>
+
 </body>
 
 </html>
