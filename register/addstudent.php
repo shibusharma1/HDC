96ffcebef83c8c1f59e2651a8155fa8d58e53ea8
kajol3 <mehtakajol21@gmail.com>
2024-07-09 00:45:18 +0545

    change in contact

---
+++ register/addstudent.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Candidate Form</title>
+</head>
+<body>
+<div class="container">
+   <div class="wrapper">
+    <form action="" method="POST"></form>
+
+    <div class="input-box">
+          <label for="name">Name</label><br></br>
+            <input type="name" placeholder="name" name="name" required>
+      </div></br>
+      <div class="input-box">
+          <label for="program">Program</label><br></br>
+            <input type="program" placeholder="program" name="program" required>
+      </div></br>
+      <div class="input-box">
+        <label for="semester">semester</label><br></br>
+          <input type="semester" placeholder="semester" name="semester" required>
+      </div></br>
+      <button type="submit" class="btn">Add</button>
+    
+</body>
+</html>
