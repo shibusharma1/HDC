91ae641f696b1b642ab4cb7d5bf4c4c3e307b310
shibusharma1 <shibusharma807@gmail.com>
2024-07-10 22:28:29 +0545

    CSS in contact.php applied

--- contact.php
+++ contact.php
@@ -3,70 +3,61 @@ include_once 'includes/header.php';
 ?>
 
 <div class="contact-map">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3572.226450438785!2d87.27589977520749!3d26.448428576926997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ef744969ab93ff%3A0xcdfeaa99bb5e0ce7!2sHimalaya%20Darshan%20College!5e0!3m2!1sen!2snp!4v1720380227436!5m2!1sen!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3572.226450438785!2d87.27589977520749!3d26.448428576926997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ef744969ab93ff%3A0xcdfeaa99bb5e0ce7!2sHimalaya%20Darshan%20College!5e0!3m2!1sen!2snp!4v1720380227436!5m2!1sen!2snp" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
-<div class="table-title">
-    <h2> Contact Details
 
-    </h2>
+<div class="table-container">
+    <div class="table-title">
+        <h2>Contact Details</h2>
+    </div>
+    <div class="table-content">
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Designation</th>
+                    <th>Contact</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Bharat Sapkota</td>
+                    <td>Campus Guest</td>
+                    <td><a href="tel:9852034345">9852034345</a></td>
+                </tr>
+                <tr>
+                    <td>Sumit Kumar Shah</td>
+                    <td>HOD, IT</td>
+                    <td><a href="tel:9849120255">9849120255</a></td>
+                </tr>
+                <tr>
+                    <td>Bhupal Mishra</td>
+                    <td>HOD, Management</td>
+                    <td><a href="tel:9842045860">9842045860</a></td>
+                </tr>
+                <tr>
+                    <td>Kushal Niroula</td>
+                    <td>Academic Head</td>
+                    <td><a href="tel:9851162059">9851162059</a></td>
+                </tr>
+                <tr>
+                    <td>Anju Bhujel</td>
+                    <td>HOD, HM</td>
+                    <td><a href="tel:9805337270">9805337270</a></td>
+                </tr>
+                <tr>
+                    <td>Leshant Sapkota</td>
+                    <td>Public Relation Officer</td>
+                    <td><a href="tel:9852035374">9852035374</a></td>
+                </tr>
+                <tr>
+                    <td>College Landline</td>
+                    <td colspan="2"><a href="tel:021-590571">021-590571</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
-<div class="table-content"></div>
-<table border= "1px solid black" cellspacing="0" cellpadding="5px">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Designation</th>
-            <th>Contact</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Bharat Sapkota</td>
-            <td>Campus Guest</td>
-            <td><a href="tel:9852034345">9852034345</a></td>
-
-        </tr>
-        <tr>
-            <td>Sumit Kumar Shah</td>
-            <td>HOD,IT</td>
-            <td><a href="tel:9849120255">9849120255</a></td>
-
-        </tr>
-        <tr>
-            <td>Bhupal Mishra</td>
-            <td>HOD,Management</td>
-            <td><a href="tel:9842045860">9842045860</a></td>
-
-        </tr>
-        <tr>
-            <td>Kushal Niroula</td>
-            <td>Academic Head</td>
-            <td><a href="tel:9851162059">9851162059</a></td>
-
-        </tr>
-        <tr>
-            <td>Anju Bhujel</td>
-            <td>HOD,HM</td>
-            <td><a href="tel:9805337270">9805337270</a></td>
-
-        </tr>
-        <tr>
-            <td>Leshant Sapkota</td>
-            <td>Public Relation Officer</td>
-            <td><a href="tel:9852035374">9852035374</a></td>
-
-        </tr>
-        <tr>
-            <td>College Landline</td>
-
-            <td colspan="2"><a href="tel:021-590571">021-590571</a></td>
-
-        </tr>
-
-    </tbody>
-    
-    
-</table>
 
 <?php
 include_once 'includes/footer.php';
