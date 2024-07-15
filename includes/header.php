975be4f8536b200e14f51d2831d5d9c3ad90f0e8
shibusharma1 <shibusharma807@gmail.com>
2024-07-15 20:33:31 +0545

    Changes in dashboard maid

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard | HDC</title>
     <link rel="stylesheet" href="./styles.css">
-    </head>
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
 <body>
 
     <div class="logo-contact">
@@ -17,20 +22,31 @@
 
         </div>
         <div class="contacts">
-            <ul>
+            <ul class="header-top">
                 <li>
-                    <p>Email:</p>
-                    <a href="mailto:himalayadarshan5@gmail.com">himalayadarshan5@gmail.com</a>
+                    <div class="icon-margin">
+                        <i class="fa-solid fa-envelope text-color"></i>
+                    </div>
+                    <div class="header-top-text">
+                        <p> Email:</p>
+                        <a href="mailto:himalayadarshan5@gmail.com">himalayadarshan5@gmail.com</a>
+                    </div>
                 </li>
                 <li>
-                    <p>Phone Number:</p>
-                    <a href="tel:021-590471">021-590471</a>
+                    <div class="icon-margin">
+                        <i class="fa-solid fa-phone text-color"></i>
+                    </div>
+                    <div class="header-top-text">
+                        <p>Phone Number:</p>
+                        <a href="tel:021-590471">021-590471</a>
+
+                    </div>
                 </li>
             </ul>
         </div>
     </div>
-    <br>
-    <nav class="navbar">
+    <!-- <br> -->
+    <nav class="navbar header-navbar">
         <ul class="navbar-links">
             <li><a href="./index.php">Home</a></li>
             <li><a href="./registercmat.php">Register</a></li>
@@ -40,5 +56,3 @@
         </ul>
     </nav>
     </div>
-
-
