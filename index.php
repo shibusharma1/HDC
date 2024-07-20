e488a965046f382a0de469fe49e64c79a52d757f
shibusharma1 <shibusharma807@gmail.com>
2024-07-20 19:48:28 +0545

    no change

--- index.php
+++ index.php
@@ -21,109 +21,109 @@ include_once 'includes/header.php';
     </div>
 </div>
 
-    <div class="program-title">
-        <p> STUDENT TESTIMONIAL</p>
-    </div>
-    <div class="program-section"> 
-        <div style="display:flex;padding:0 10rem;">
-            <div class="std-img">
-                <img src="assets/limadidi.png" alt="limadidi" width="350px">
-            </div>
-            <div class="std-description" style="display:flex; flex-direction:column;justify-content:center;">
-                <p class="std-name" style="font-size:2.5rem;"> Lima Sapkota</p>
-                
-                <br>
-                Himalaya Darshan College is the place where student voices are heard and counted. It is difficult to sum
-                up the memories and experience of four years in a few lines. HDC has helped me to develop a positive
-                attitude towards my studies and discover more about myself. Apart from my study, I have been involved
-                with various community groups where I used my academic and professional skills to yield good outcomes. I
-                have grown both personally and professionally.
-
-            </div>
+<div class="program-title">
+    <p> STUDENT TESTIMONIAL</p>
+</div>
+<div class="program-section">
+    <div style="display:flex;padding:0 10rem;">
+        <div class="std-img">
+            <img src="assets/limadidi.png" alt="limadidi" width="350px">
         </div>
-    </div>
-    <div class="program-title">
-        <p> Programs We Offer
-    </div> 
-    <div class="program-section">
-        <div class="box1 box">
-            <div class="box-content">
-                <h2>BIM</h2>
-                <div class="box-img" style="background-image:url('assets/bim.png');"></div>
-            </div>
-        </div>
-        <div class="box2 box">
-            <div class="box-content">
-                <h2>BCA</h2>
-                <div class="box-img" style="background-image: url('assets/bca.png');"></div>
+        <div class="std-description" style="display:flex; flex-direction:column;justify-content:center;">
+            <p class="std-name" style="font-size:2.5rem;"> Lima Sapkota</p>
 
-            </div>
-        </div>
-        <div class="box3 box">
-            <div class="box-content">
-                <h2>Bsc. CSIT</h2>
-                <div class="box-img" style="background-image: url('assets/bsccsit.png');"></div>
+            <br>
+            Himalaya Darshan College is the place where student voices are heard and counted. It is difficult to sum
+            up the memories and experience of four years in a few lines. HDC has helped me to develop a positive
+            attitude towards my studies and discover more about myself. Apart from my study, I have been involved
+            with various community groups where I used my academic and professional skills to yield good outcomes. I
+            have grown both personally and professionally.
 
-            </div>
-        </div>
-        <div class="box4 box">
-            <div class="box-content">
-                <h2>BHM</h2>
-                <div class="box-img" style="background-image: url('assets/bhm.png');"></div>
-
-            </div>
-        </div>
-        <div class="box5 box">
-            <div class="box-content">
-                <h2>BSS</h2>
-                <div class="box-img" style="background-image: url('assets/bbs.png');"></div>
-
-            </div>
         </div>
     </div>
-    <br>
-
-    <!-- Tie-up Sections -->  
-       <div class="tie-up-title">
-        <p>TIE-UPS & MOUS</p>
-    </div> 
-    <div class="tie-up-section">
-        <div class="box1 tie-up">
-            <div class="box-content">
-                
-                <div class="tie-up-img" style="background-image:url('assets/tie1.png');"></div>
-            </div>
-        </div>
-        <div class="box2 tie-up">
-            <div class="box-content"> 
-                <div class="tie-up-img" style="background-image: url('assets/tie2.gif');"></div>
-            </div>
-        </div>
-        <div class="box3 tie-up">
-            <div class="box-content">
-                <div class="tie-up-img" style="background-image: url('assets/tie3.jpg');"></div>
-
-            </div>
-        </div>
-        <div class="box4 tie-up">
-            <div class="box-content">
-                <div class="tie-up-img" style="background-image: url('assets/tie4.png');"></div>
-
-            </div>
-        </div>
-        <div class="box5 tie-up">
-            <div class="box-content">
-                <div class="tie-up-img" style="background-image: url('assets/tie5.png');"></div>
-            </div>
-            </div>
-        <div class="box6 tie-up">
-            <div class="box-content">
-                <div class="tie-up-img" style="background-image: url('assets/tie6.png');"></div>
-
-            </div>
+</div>
+<div class="program-title">
+    <p> Programs We Offer
+</div>
+<div class="program-section">
+    <div class="box1 box">
+        <div class="box-content">
+            <h2>BIM</h2>
+            <div class="box-img" style="background-image:url('assets/bim.png');"></div>
         </div>
     </div>
-    
+    <div class="box2 box">
+        <div class="box-content">
+            <h2>BCA</h2>
+            <div class="box-img" style="background-image: url('assets/bca.png');"></div>
+
+        </div>
+    </div>
+    <div class="box3 box">
+        <div class="box-content">
+            <h2>Bsc. CSIT</h2>
+            <div class="box-img" style="background-image: url('assets/bsccsit.png');"></div>
+
+        </div>
+    </div>
+    <div class="box4 box">
+        <div class="box-content">
+            <h2>BHM</h2>
+            <div class="box-img" style="background-image: url('assets/bhm.png');"></div>
+
+        </div>
+    </div>
+    <div class="box5 box">
+        <div class="box-content">
+            <h2>BSS</h2>
+            <div class="box-img" style="background-image: url('assets/bbs.png');"></div>
+
+        </div>
+    </div>
+</div>
+<br>
+
+<!-- Tie-up Sections -->
+<div class="tie-up-title">
+    <p>TIE-UPS & MOUS</p>
+</div>
+<div class="tie-up-section">
+    <div class="box1 tie-up">
+        <div class="box-content">
+
+            <div class="tie-up-img" style="background-image:url('assets/tie1.png');"></div>
+        </div>
+    </div>
+    <div class="box2 tie-up">
+        <div class="box-content">
+            <div class="tie-up-img" style="background-image: url('assets/tie2.gif');"></div>
+        </div>
+    </div>
+    <div class="box3 tie-up">
+        <div class="box-content">
+            <div class="tie-up-img" style="background-image: url('assets/tie3.jpg');"></div>
+
+        </div>
+    </div>
+    <div class="box4 tie-up">
+        <div class="box-content">
+            <div class="tie-up-img" style="background-image: url('assets/tie4.png');"></div>
+
+        </div>
+    </div>
+    <div class="box5 tie-up">
+        <div class="box-content">
+            <div class="tie-up-img" style="background-image: url('assets/tie5.png');"></div>
+        </div>
+    </div>
+    <div class="box6 tie-up">
+        <div class="box-content">
+            <div class="tie-up-img" style="background-image: url('assets/tie6.png');"></div>
+
+        </div>
+    </div>
+</div>
+
 
 <?php
 include_once 'includes/footer.php';
